c7c7d3295f33bc4fe2c3e593791ef4119f5a36ee
Álvaro Giovanni <alvaro.qzlopes123@gmail.com>
2026-06-06 13:30:07 -0300

    chore: api configuration

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+
+const app = express();
+
+app.use(helmet());
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
+
+app.use(express.json({ limit: '10mb' }));
+
+app.use(express.urlencoded({ extended: true }));
+
+module.exports = app;
